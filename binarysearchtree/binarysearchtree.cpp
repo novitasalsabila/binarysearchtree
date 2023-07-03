d1b7ad9e41cdb7dfe1d708945c935a40367f982f
novitasalsabila <novnovv10@gmail.com>
2023-07-03 20:40:45 +0700

    Membuat case1 , variabel dan memanggil object insert

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -135,5 +135,15 @@ int main()
         cin >> ch;
         cout << endl;
 
+        switch (ch)
+        {
+        case'1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
        
 }
