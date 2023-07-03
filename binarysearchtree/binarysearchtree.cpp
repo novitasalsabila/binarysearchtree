6938da5b6bebe8b1e60c83ba8119a6bfbdaa7678
novitasalsabila <novnovv10@gmail.com>
2023-07-03 20:20:23 +0700

    Jika tree kosong, simpul baru akan dijadikan akar. Jika element lebih kecil dari nilai simpul saat ini, simpul baru akan ditempatkan sebagai anak sebelah kiri. Jika element lebih besar, simpul baru akan ditempatkan sebagai anak sebelah kanan.

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -37,4 +37,22 @@ public:
         search(element, parent, currentNode); //Locate the node which will be the parent of
         //the node to be inserted
 
-        
+        if (parent == NULL) //If the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; //Mark the new node as ROOT
+            return; //Exit
+        }
+
+        if (element < parent->info) //If the value in the data field of the new node is less
+            //than that of the parent
+        {
+            parent->leftchild = newNode; //Make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) //If the value in the data field of the new node is
+            //greater than that of the parent
+        {
+            parent->rightchild = newNode; //Make the right child of the parent point to the new node
+        }
+    }
+
+    
